ee5c92cd5661227feec8ae79081141a64a531063
Victor-Eduardo <victorelalves@gmail.com>
2023-05-25 21:14:26 -0300

    Finalizando as funcionalidades exigidas no desafio

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,85 @@
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
 import styles from './App.module.css';
 
+import { v4 as uuidv4 } from 'uuid';
+
 import { Header } from './components/Header';
-import { Task } from './components/Task';
+import { Task, TaskType } from './components/Task';
 
 import { PlusCircle, ClipboardText } from '@phosphor-icons/react';
 
 export function App() {
+  const [tasks, setTasks] = useState<TaskType[]>([]);
+
+  const [newTaskContent, setNewTaskContent] = useState('');
+
+  const totalDoneTasks = tasks.reduce( (allDones, task) => {
+    if(task.done === true) { allDones++ }
+    return allDones;
+   }, 0 ); 
+
+  const totalCreatedTasks = tasks.length;
+
+  function handleCreateNewTask(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const task: TaskType = {
+      id: uuidv4(),
+      content: newTaskContent,
+      done: false
+    };
+
+    setTasks([
+      ...tasks,
+      task
+    ]);
+
+    setNewTaskContent('');
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskContent(event.target.value);
+
+    event.target.setCustomValidity('');
+  }
+
+  function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('Este campo é obrigatório!');
+  }
+
+  function deleteTask(taskId: string) {
+    const tasksWithoutDeletedOne = tasks.filter(task => {
+      return task.id !== taskId
+    });
+
+    setTasks(tasksWithoutDeletedOne);
+  }
+
+  function markDoneTask(taskId: string) {
+    const newTasks = tasks.map((task => { 
+      if(task.id === taskId)  return { ...task, done: !task.done };
+      return {
+        ...task
+      };
+     }));
+
+     setTasks(newTasks);
+  }
+
   return(
     <div>
       <Header />
 
-      <form className={styles.taskForm}>
-        <input type="text" placeholder="Adicione uma nova tarefa" />
+      <form onSubmit={handleCreateNewTask} className={styles.taskForm}>
+        <input 
+          name="task" 
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+          required
+          value={newTaskContent}
+          onChange={handleNewTaskChange}
+          onInvalid={handleNewTaskInvalid}
+        />
         <button type="submit">
           Criar
           <PlusCircle size={18} />
@@ -21,29 +89,30 @@ export function App() {
       <div className={styles.wrapper}>
         <header className={styles.tasksInfo}>
           <span className={styles.created}>
-            Tarefas criadas {' '}
-            <span className={styles.taskCounter}>5</span>
+            Tarefas criadas
+            <span className={styles.taskCounter}>{totalCreatedTasks}</span>
           </span>
 
           <span className={styles.done}>
-            Concluídas {' '}
-            <span className={styles.taskCounter}>0</span>
+            Concluídas
+            <span className={styles.taskCounter}>{totalDoneTasks} de {totalCreatedTasks}</span>
           </span>
         </header>
 
-        {/* <div className={styles.emptyItens}>
-          <ClipboardText size={60} />
-          <span>
-            <strong>Você ainda não tem tarefas cadastradas</strong>
-            <p>Crie tarefas e organize seus itens a fazer</p>
-          </span>
-        </div> */}
+        <div className={tasks.length === 0 ? styles.emptyItens : styles.hidden}>
+            <ClipboardText size={60} />
+            <span>
+              <strong>Você ainda não tem tarefas cadastradas</strong>
+              <p>Crie tarefas e organize seus itens a fazer</p>
+            </span>
+        </div>
 
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task /> 
+
+        {
+          tasks.map((task) => {
+            return <Task key={task.content} task={task} onDeleteTask={deleteTask} onMarkDoneTask={markDoneTask} />
+          })
+        }
       </div>
     </div>
   );
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,16 +1,38 @@
+import { HTMLAttributes, useId } from 'react';
 import style from './Task.module.css';
 
 import { Trash } from '@phosphor-icons/react';
 
-export function Task() {
+export interface TaskType {
+  id: string;
+  content: string;
+  done: boolean;
+}
+
+interface TaskProps {
+  task: TaskType;
+  onDeleteTask: (id: string) => void;
+  onMarkDoneTask: (id: string) => void;
+}
+
+export function Task({ task, onDeleteTask, onMarkDoneTask }: TaskProps){
+  const inputId = useId();
   
+  function handleDeleteTask() {
+    onDeleteTask(task.id);
+  }
+
+  function handleDoneTask() {
+    onMarkDoneTask(task.id);
+  }
+
   return(
     <div className={style.task}>
-      <label>
-        <input type="checkbox" />
-        <span>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</span>
+      <input type="checkbox" id={inputId} onClick={handleDoneTask} />
+      <label htmlFor={inputId}>
+        <span>{ task.content }</span>
       </label>
-      <button>
+      <button onClick={handleDeleteTask}>
         <Trash size={18} />
       </button>
     </div>
